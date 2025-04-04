f715ecdca1af104356cc8455c1ab814dc16eda74
sohad nassar <sohad.nassar@tentimejo.com>
2025-04-04 10:32:43 +0200

    delete confirmation

--- productmanagement/src/components/product/ProductTable.jsx
+++ productmanagement/src/components/product/ProductTable.jsx
@@ -2,17 +2,16 @@ import React, { useEffect, useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 import { Space, Table, Button, Typography, Popconfirm, notification } from 'antd';
 import { CreateOrUpdate } from './CreateOrUpdate'
+import Swal from 'sweetalert2'
 const key = 'updatable';
 
 export const ProductTable = () => {
-    const [show1, setShow1] = useState(false); 
-    const [msg, setMsg] = useState(""); 
+    const [show1, setShow1] = useState(false);  
     const [id, setId] = useState(null); 
     const [data, setData] = useState(JSON.parse(localStorage.getItem('products')) || []);
 
     const [api, contextHolder] = notification.useNotification();
     const openNotification = (m) => {
-    
         setTimeout(() => {
             api.success({
                 key,
@@ -21,7 +20,22 @@ export const ProductTable = () => {
             });
         }, 1000);
     };
-
+    const handleDelete = (key) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                deleteItem(key);
+               
+            }
+        });
+    }
     const handleCloseModal = () => {
         setShow1(false); 
     }
@@ -34,7 +48,11 @@ export const ProductTable = () => {
 
     const deleteItem = (key) => {
         setData((lst) => lst.filter((item) => item.key !== key));
-        openNotification("Deleted product succssfully.");
+        Swal.fire({
+            title: "Deleted!",
+            text: "Deleted product succssfully.",
+            icon: "success"
+        });
     }
     const editItem = (newProduct) => {
         setData((lst) => lst.map((currentRow, idx) => {
@@ -100,9 +118,10 @@ export const ProductTable = () => {
                         Edit
                     </Button>
                   
-                    <Popconfirm title="Delete?" onConfirm={() => deleteItem(record.key)}>
-                        <Button color="danger" variant="outlined">Delete</Button>
-                    </Popconfirm>
+                    <Button color="danger" variant="outlined" onClick={() => {
+                        handleDelete(record.key)
+                }}>Delete</Button>
+
                    
                 </Space>
             ),
