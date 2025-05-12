4f082b9b221d8e4eb21a3c57515b02294c9552c9
sohad nassar <sohad.nassar@tentimejo.com>
2025-05-12 16:35:49 +0300

    products page

--- productmanagement/src/components/AppHeader.jsx
+++ productmanagement/src/components/AppHeader.jsx
@@ -28,6 +28,5 @@ export const AppHeader = () => {
                 )}
             </Space>
         </div>
-
     );
 }; 
--- productmanagement/src/pages/Products.jsx
+++ productmanagement/src/pages/Products.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
-import {  Typography } from 'antd';
+import { Typography } from 'antd';
+import { Item } from "../components/product/Item"
+import "../css/products.css" 
 
 export const Products = () => {
-   
-    return (
-        <>
+    const [data, setData] = useState(JSON.parse(localStorage.getItem('products')) || []);
 
-            <Typography.Title level={4}> Products</Typography.Title>
-             
-      </>
+    return (
+        <div className="products">
+            <h1> latest product</h1>
+        <hr />
+            <div className="product-item">
+                {data.map((item,i) => {
+                    return <Item key={i} id={item.key} name={item.name} image={item.img} price={item.price} />
+                }) }
+            </div>
+        </div>
     );
 }; 
  
